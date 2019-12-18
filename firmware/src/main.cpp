6091781cb6e21a24ae3405b5100a1093d6deb5d1
Priit Kallas <kallaspriit@gmail.com>
2019-12-18 12:59:00 +0200

    Improved firmware and case

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,7 +1,8 @@
 /*
  * 1. Choose correct board (Tools > Board > "Teensy 3.2/3.1")
  * 2. Choose Serial + HID USB type (USB Type > "Serial + Keyboard + Mouse + Joystick")
- * 3. Edit arduino/hardware/teensy/avr/cores/teensy3/usb_desc.h and under "#elif defined(USB_SERIAL_HID)"
+ * 3. [ONLY FOR EXTREME 16 bit joystick]
+ *    Edit arduino/hardware/teensy/avr/cores/teensy3/usb_desc.h and under "#elif defined(USB_SERIAL_HID)"
  *    change "#define JOYSTICK_SIZE 12" to "#define JOYSTICK_SIZE 64" which enables 16 bit extreme joystick
  *    (or C:\Users\kalla\.platformio\packages\framework-arduinoteensy\cores\teensy3 when using platformio)
  */
@@ -9,21 +10,22 @@
 #include <Arduino.h>
 
 // configuration
-const int SENSOR_PIN = A7;         // pressure sensor or potentiometer
-const int LOOP_FREQUENCY_HZ = 100; // Hz
-const int HEADROOM_PERCENTAGE = 1;
-const int INPUT_RESOLUTION_BITS = 16;
-const int OUTPUT_RESOLUTION_BITS = 16;
+const int SENSOR_PIN = A7;             // pressure sensor or potentiometer
+const int LOOP_FREQUENCY_HZ = 100;     // Hz
+const int DEADZONE_PERCENTAGE = 1;     // how much deadzone to apply at either end
+const int INPUT_RESOLUTION_BITS = 16;  // analog input resolution in bits
+const int OUTPUT_RESOLUTION_BITS = 10; // normal joystick
+const int CONSOLE_INTERMAL_MS = 100;   // log to console interval (set to zero to disable)
+// const int OUTPUT_RESOLUTION_BITS = 16; // extreme joystick (requires usb_desc.h modification)
 
 // calculated constants
 const int LOOP_DELAY_MS = 1000 / LOOP_FREQUENCY_HZ;
 const long INPUT_RANGE = pow(2, INPUT_RESOLUTION_BITS);
 const long OUTPUT_RANGE = pow(2, OUTPUT_RESOLUTION_BITS);
-const long HEADROOM = floor((HEADROOM_PERCENTAGE / 100.0f) * INPUT_RANGE);
+const long DEADZONE = floor((DEADZONE_PERCENTAGE / 100.0f) * OUTPUT_RANGE);
 
-// auto-calibration info
-long smallestRawValue = INPUT_RANGE * 0.25L;
-long largestRawValue = INPUT_RANGE * 0.75L;
+// console timer
+long lastConsoleTime = 0;
 
 // setup app
 void setup()
@@ -34,16 +36,20 @@ void setup()
   // setup pins
   pinMode(SENSOR_PIN, INPUT);
 
+  // setup manual send mode
+  Joystick.useManualSend(true);
+
   // send startup info
   Serial.begin(115200);
-  Serial.print("Starting joystick at ");
+
+  // send startup info after a small delay
+  delay(3000);
+
+  Serial.print("Starting Priit Sim Racing Handbrake v1.6.0 at ");
   Serial.print(OUTPUT_RESOLUTION_BITS);
   Serial.print(" bits resolution, running at ");
   Serial.print(LOOP_FREQUENCY_HZ);
   Serial.println("Hz");
-
-  // setup manual send mode
-  Joystick.useManualSend(true);
 }
 
 // main loop
@@ -52,45 +58,38 @@ void loop()
   // read current value
   long inputValue = analogRead(SENSOR_PIN);
 
-  // auto-calibrate smallest value
-  if (inputValue < smallestRawValue)
-  {
-    smallestRawValue = inputValue;
-  }
-
-  // auto-calibrate largest value
-  if (inputValue > largestRawValue)
-  {
-    largestRawValue = inputValue;
-  }
-
-  // map raw value to output range (use calibration values)
-  // long outputValue = map(inputValue, smallestRawValue, largestRawValue, 0L, OUTPUT_RANGE - 1L);
-  // long outputValue = map(inputValue, 0L, INPUT_RANGE - 1L, 0L, OUTPUT_RANGE - 1L);
-  long outputValue = inputValue;
+  // map input value to output range
+  long outputValue = map(inputValue, 0L, INPUT_RANGE - 1L, 0L, OUTPUT_RANGE - 1L);
 
   // cut of small proportion of the low and high end to be able to always hit the limits
-  if (outputValue < HEADROOM)
+  if (outputValue < DEADZONE)
   {
     outputValue = 0L;
   }
-  else if (outputValue > OUTPUT_RANGE - 1L - HEADROOM)
+  else if (outputValue > OUTPUT_RANGE - 1L - DEADZONE)
   {
     outputValue = OUTPUT_RANGE - 1L;
   }
 
-  // Serial.print("Input value: ");
-  // Serial.println(inputValue);
-  // Serial.print("Smallest raw value: ");
-  // Serial.println(smallestRawValue);
-  // Serial.print("Largest raw value: ");
-  // Serial.println(largestRawValue);
-  // Serial.print("Output value: ");
-  // Serial.println(outputValue);
-  // Serial.println();
+  // get current time
+  long currentTime = millis();
+
+  // log to console at certain interval (set CONSOLE_INTERMAL_MS to zero to disable)
+  if (CONSOLE_INTERMAL_MS > 0 && (lastConsoleTime == 0 || currentTime - lastConsoleTime > CONSOLE_INTERMAL_MS))
+  {
+    // detailed output
+    Serial.print(inputValue);
+    Serial.print("/");
+    Serial.println(outputValue);
+
+    lastConsoleTime = currentTime;
+  }
 
   // set joystick value
   Joystick.Z(outputValue);
+
+  // set a button value at mid range
+  Joystick.button(1, outputValue > OUTPUT_RANGE / 2);
 
   // send all updates
   Joystick.send_now();
--- firmware/src/name.c
+++ firmware/src/name.c
@@ -7,11 +7,11 @@
 // Edit these lines to create your own name.  The length must
 // match the number of characters in your custom name.
 
-#define JOYSTICK_NAME                           \
-  {                                             \
-    'H', 'a', 'n', 'd', 'b', 'r', 'a', 'k', 'e' \
+#define JOYSTICK_NAME                                          \
+  {                                                            \
+    'S', 'L', ' ', 'H', 'a', 'n', 'd', 'b', 'r', 'a', 'k', 'e' \
   }
-#define JOYSTICK_NAME_LEN 9
+#define JOYSTICK_NAME_LEN 12
 
 // Do not change this part. This exact format is required by USB.
 
